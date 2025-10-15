49fcf497dc72df8779541bd705d1f0820b41cf96
afrzzz <afrizalardiansyah2001@gmail.com>
2025-10-15 19:01:14 +0700

    benerin bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Card from "./Card";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/card" element={<Card />} />
     </Routes>
   </BrowserRouter>
 );
